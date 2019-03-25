4d0af1a61e37d9e941fbae4e365c4ad6408ca07b
fallred <fallred@sina.com>
2019-03-25 18:00:54 +0800

    受保护路由1

--- src/containers/Mine/index.tsx
+++ src/containers/Mine/index.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react';
-export default class Mine extends React.Component {
+import {connect} from 'react-redux';
+import {Store,Session} from '../../types';
+import {Redirect} from 'react-router-dom';
+interface IProps {
+    user:any
+}
+class Mine extends React.Component<IProps> {
     render () {
         return (
-            <div>mine</div>
+            this.props.user ? <div>mine</div> : <Redirect to="/login"/>
+            
         );
     }
 }
+export default connect(
+    (state:Store):Session => state.session
+)(Mine)
